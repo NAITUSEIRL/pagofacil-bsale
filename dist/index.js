c858b2c82d3869d4bcdf05e6d54152185b828cc2
Cristian Tala <yomismo@cristiantala.cl>
2018-04-28 11:31:11 -0300

    Exporting Classes

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Client_1 = require("./Models/Client");
+const Detail_1 = require("./Models/Detail");
+const References_1 = require("./Models/References");
+const DocumentSII_1 = require("./Models/DocumentSII");
+module.exports = { Client: Client_1.Client, Detail: Detail_1.Detail, References: References_1.References, DocumentSII: DocumentSII_1.DocumentSII };
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Client } from "./Models/Client";
+import { Detail } from "./Models/Detail";
+import { References } from "./Models/References";
+import { DocumentSII } from "./Models/DocumentSII";
+
+
+module.exports = {Client, Detail, References, DocumentSII};
